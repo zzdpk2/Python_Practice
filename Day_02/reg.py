ba65ad87f25277af29eeaee98e337f459d9816b1
zzdpk2 <zzdpk2@126.com>
2020-09-24 01:26:12 +0800

    learning regular expression

--- Day_02/reg.py
+++ Day_02/reg.py
@@ -15,11 +15,15 @@ import re
 # reg = "hello"
 # print(re.findall(reg, s))
 
-s = "fsad_as_das是范德萨@~#&范德萨  sfs_d   f1_232 "
-print(re.findall("\w", s))
-print(re.findall("\d", s))
-print(re.findall("\s", s))
-print(re.findall("^f", s))
+# s = "fsad_as_das是范德萨@~#&范德萨  sfs_d   f1_232 "
+# print(re.findall("\w", s))
+# print(re.findall("\d", s))
+# print(re.findall("\s", s))
+# print(re.findall("^f", s))
 
-#django url匹配使用正则
+# django url匹配使用正则
+
+s = "hhhsda1312....说的很对4575643是开发环境dsfsdf....sas154534s"
+print(re.findall("\d{3}", s))
+print(re.findall("[\da-z]+", s))
 
