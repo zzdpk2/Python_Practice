85a24456b3b7e08bc3e5f4f6edb4c117a9c2c05f
zzdpk2 <zzdpk2@126.com>
2020-10-02 22:10:51 +0800

    regex update

--- Day_02/reg.py
+++ Day_02/reg.py
@@ -27,3 +27,17 @@ s = "hhhsda1312....说的很对4575643是开发环境dsfsdf....sas154534s"
 print(re.findall("\d{3}", s))
 print(re.findall("[\da-z]+", s))
 
+s1 = "我的qq是33454312"
+reg = re.compile("\d{5,12}")
+print(re.findall(reg, s1))
+
+# 分组匹配
+s = "我的qq是:33454312, 我的邮编是:310000"
+reg = re.compile("(\d{5,12}).*(\d{6})")
+# 不匹配，因为分组识别失败
+# reg = re.compile("(\d{5,12}).*(\d{6})")
+# print(re.findall(reg, s))
+print(re.search(reg, s).group())
+print(re.search(reg, s).group(0))
+print(re.search(reg, s).group(1))
+print(re.search(reg, s).group(2))
