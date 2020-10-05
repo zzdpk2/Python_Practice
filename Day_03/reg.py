e2fd9c87b8c1fae42e809308c126a89b00922368
zzdpk2 <zzdpk2@126.com>
2020-10-05 15:26:07 +0800

    new

--- Day_03/reg.py
+++ Day_03/reg.py
@@ -9,3 +9,6 @@ s = "greedyaiiiiii"
 reg = "greedyai+?"
 print(re.findall(reg, s))
 
+s = "我的电话号码是: 010-89832343 0431-89847345  0432-7842342"
+reg = "0\d{2}-\d{8}|0\d{3}-\d{8}|0\d{3}-\d{7,8}"
+print(re.findall(reg, s))
