dffe79d0e69f0f61605182c3ad8b7a7be5a0a73a
zzdpk2 <zzdpk2@126.com>
2020-10-07 20:55:46 +0800

    building quick sort

---
+++ Day_04/qsort.py
@@ -0,0 +1,8 @@
+def qsort(data:list, left:int, right:int) -> None:
+    if left >= right:
+        return
+    pivot = data[left]
+    i = left
+    j = right
+    while i != j:
+        while j > i and data[]
