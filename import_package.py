c2d91ca1f08e35827d41ef196ff2c3b0968c83bf
zzdpk2 <zzdpk2@126.com>
2020-10-16 15:47:55 +0800

    import practice

--- import_package.py
+++ import_package.py
@@ -1,1 +1,6 @@
-from package_import_practice. import
+# from package_import_practice import module
+from package_import_practice.module import ImportClass, my_print
+
+my_print()
+ic = ImportClass
+ic.my_import_class_print()
---
+++ package_import_practice/module.py
@@ -0,0 +1,6 @@
+def my_print():
+    print("In-module functions are called!")
+
+class ImportClass:
+    def my_import_class_print(self):
+        print("In-class functions are called!")
