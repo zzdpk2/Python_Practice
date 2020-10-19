ff97a4f1f998537fc1001ce46085de464160883b
zzdpk2 <zzdpk2@126.com>
2020-10-19 17:20:11 +0800

    decorator and closure

--- Day_07/decorator.py
+++ Day_07/decorator.py
@@ -1,18 +1,36 @@
 # decorator
+import time
 
 
-def func_1(x):
-    return x * 2
+# def func_1(x):
+#     return x * 2
+#
+#
+# def func_2(x):
+#     return x * 3
+#
+#
+# def func_3(x, y, i, j):
+#     return x(i) + y(j)
+#
+#
+# r = func_3(func_1, func_2, 2, 3)
+# print(r)
 
 
-def func_2(x):
-    return x * 3
+def runtime_noargs(function_name):
+    def wrapper():
+        start_time = time.time()
+        function_name()
+        end_time = time.time()
+        print(end_time - start_time)
+    return wrapper
 
 
-def func_3(x, y, i, j):
-    return x(i) + y(j)
+@runtime_noargs
+def function_demo1():
+    print("demo1 is running!")
 
 
-r = func_3(func_1, func_2, 2, 3)
-print(r)
+function_demo1()
 
